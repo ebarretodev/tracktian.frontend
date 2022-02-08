23a9bab8bc5c0d276f3d07db51a4e85f50aff243
Eliabel Barreto <67025189+ebarretodev@users.noreply.github.com>
2022-02-07 23:28:21 -0300

    Correction on routes to get string direct to useParams

--- src/helpers/LocalApi.ts
+++ src/helpers/LocalApi.ts
@@ -11,19 +11,19 @@ const localApi={
     postUser: (data: any ) => { return axios.post(`${BASE_URL_API}/users`, data)},
 
     getCompanies: () => { return axios.get(`${BASE_URL_API}/companies`)},
-    getCompany: (id: number) => { return axios.get(`${BASE_URL_API}/companies/${id}`)},
+    getCompany: (id: string) => { return axios.get(`${BASE_URL_API}/companies/${id}`)},
     putCompany: (id: number, values: any) => { return axios.put(`${BASE_URL_API}/companies/${id}`, values)},
     deleteCompany: (id: number) => { return axios.delete(`${BASE_URL_API}/companies/${id}`)},
     postCompany: (data: any ) => { return axios.post(`${BASE_URL_API}/companies`, data)},
 
     getUnits: () => { return axios.get(`${BASE_URL_API}/units`)},
-    getUnit: (id: number) => { return axios.get(`${BASE_URL_API}/units/${id}`)},
+    getUnit: (id: string) => { return axios.get(`${BASE_URL_API}/units/${id}`)},
     putUnit: (id: number, values: any) => { return axios.put(`${BASE_URL_API}/units/${id}`, values)},
     deleteUnit: (id: number) => { return axios.delete(`${BASE_URL_API}/units/${id}`)},
     postUnit: (data: any ) => { return axios.post(`${BASE_URL_API}/units`, data)},
 
     getAssets: () => { return axios.get(`${BASE_URL_API}/assets`)},
-    getAsset: (id: number) => { return axios.get(`${BASE_URL_API}/assets/${id}`)},
+    getAsset: (id: string) => { return axios.get(`${BASE_URL_API}/assets/${id}`)},
     putAsset: (id: number, values: any) => { return axios.put(`${BASE_URL_API}/assets/${id}`, values)},
     deleteAsset: (id: number) => { return axios.delete(`${BASE_URL_API}/assets/${id}`)},
     postAsset: (data: any ) => { return axios.post(`${BASE_URL_API}/assets`, data)},
