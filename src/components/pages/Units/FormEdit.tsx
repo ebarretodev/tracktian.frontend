3dd473c251d1e5a42168e83870de67171b1fb31b
Eliabel Barreto <67025189+ebarretodev@users.noreply.github.com>
2022-02-07 23:07:28 -0300

    Units related pages to create, edit, delete and overview

---
+++ src/components/pages/Units/FormEdit.tsx
@@ -0,0 +1,102 @@
+import React, {useState, useEffect, useReducer} from "react";
+import {Row, Col, Typography, Input, Form, Button, Radio, Switch, Slider, Select, message} from 'antd'
+import { LoadingOutlined } from '@ant-design/icons'
+import axios from "axios";
+import { useHistory, useParams } from "react-router";
+
+type ParamsTypes = {
+    id: string,
+}
+
+const {Title} = Typography
+
+const layout = {
+    labelCol: { span: 8},
+    wrapperCol: { span: 16 },
+}
+
+const FormApp = () => {
+    const [loading, setLoading] = useState(false)
+    const [loadingPage, setLoadingPage] = useState(true)
+    const [company, setCompany] = useState({
+        id: 0,
+        name: '',
+        address: '',
+        business: '',
+    })
+    const history = useHistory()
+
+    const { id } = useParams<ParamsTypes>()
+
+    useEffect(()=>{
+        axios.get(`http://localhost:5000/companies/${id}`)
+            .then(res=>{
+                setCompany(res.data)
+                setLoadingPage(false)
+            }).catch( error =>{
+                message.error(error)
+                history.push('/users')
+            })
+    }, [])
+
+    const handleSubmit = (values: any) => {
+        setLoading(true)
+        axios.put(`http://localhost:5000/users/${company.id}`,values)
+            .then(res=>{
+                setLoading(false)
+                message.success('User updated successfully!')
+                history.push('/list')
+            })
+            .catch(error => {
+                setLoading(false)
+                message.error(error)
+            })
+    }
+    return(
+    <div>
+        { loadingPage ?
+        <div></div> :
+    	<div>
+            <Row gutter={[40, 0]}>
+            <Col span={23}>
+                <Title style={{textAlign: 'center'}} level={2}>
+                Please updated the User info
+                { loadingPage ? <LoadingOutlined /> : <div></div>}
+                </Title>
+            </Col>
+            </Row>
+            <Row gutter={[40, 0]}>
+                <Col span={18}>
+                    <Form {...layout} onFinish={handleSubmit}>
+                    <Form.Item name="name" label="Company Name" initialValue={company.name} rules={[{
+                        required: true,
+                        message: 'Please input your Company name'
+                    }]} >
+                        <Input placeholder="Please enter your company name" />
+                    </Form.Item>
+                    <Form.Item name='address' label='Address' initialValue={company.address} rules={[{
+                        required:true,
+                        message: 'Please input your correct address'
+                    }]}>
+                        <Input placeholder="Please enter your address" />
+                    </Form.Item>
+                    <Form.Item name='business' label='Business Segment' initialValue={company.business} rules={[{
+                        required: true,
+                        message:'Please insert your business segment'
+                    }]} >
+                        <Input placeholder="Please insert your business segment"/>
+                    </Form.Item>
+                        <div style={{textAlign: "right"}} >
+                            <Button type="primary" loading={loading} htmlType="submit">Save</Button>{'  '}
+                            <Button type="primary" danger htmlType="button" onClick={()=>{history.push('/companies/')}}>Back</Button>
+                        </div>
+                    </Form>
+                </Col>
+            </Row>
+        </div>
+        }
+    </div>
+  );
+}
+
+export default FormApp;
---
+++ src/components/pages/Units/Units.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Empty } from 'antd'
+
+const Units = () => {
+    return(
+        <div>
+            <Empty />
+        </div>
+    )
+}
+
+export default Units
