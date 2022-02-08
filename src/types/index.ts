9f28c0946bed240721bcf979120be6cf1c9b97d9
Eliabel Barreto <67025189+ebarretodev@users.noreply.github.com>
2022-02-07 23:05:01 -0300

    Types necessary to use on site

---
+++ src/types/index.ts
@@ -0,0 +1,20 @@
+export type CompanyType = {
+    id: number,
+    name: string,
+    address: string,
+    business: string
+}
+
+export type UserType = {
+    id: number,
+    username: string,
+    email: string,
+    company:string,
+}
+
+export type ParamsTypes = {
+    id: string,
+}
+
+
+
