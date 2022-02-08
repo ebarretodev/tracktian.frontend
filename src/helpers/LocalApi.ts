89f42dbc4672ff00fa806dd2fa278927d3922ba5
Eliabel Barreto <67025189+ebarretodev@users.noreply.github.com>
2022-02-07 23:22:25 -0300

    Corrections on routes to local API

--- src/helpers/LocalApi.ts
+++ src/helpers/LocalApi.ts
@@ -5,28 +5,28 @@ const BASE_URL_API = `http://localhost:5000`
 
 const localApi={
     getUsers: ()  => { return axios.get(`${BASE_URL_API}/users`)},
-    getUser: (id: number) => { return axios.get(`${BASE_URL_API}/users/${id}`)},
-    putUser: (id: number) => { return axios.put(`${BASE_URL_API}/users/${id}`)},
+    getUser: (id: string) => { return axios.get(`${BASE_URL_API}/users/${id}`)},
+    putUser: (id: number, values: any) => { return axios.put(`${BASE_URL_API}/users/${id}`, values)},
     deleteUser: (id: number) => { return axios.delete(`${BASE_URL_API}/users/${id}`)},
-    postUser: (id: number, data: any ) => { return axios.post(`${BASE_URL_API}/users`, data)},
+    postUser: (data: any ) => { return axios.post(`${BASE_URL_API}/users`, data)},
 
     getCompanies: () => { return axios.get(`${BASE_URL_API}/companies`)},
     getCompany: (id: number) => { return axios.get(`${BASE_URL_API}/companies/${id}`)},
-    putCompany: (id: number) => { return axios.put(`${BASE_URL_API}/companies/${id}`)},
+    putCompany: (id: number, values: any) => { return axios.put(`${BASE_URL_API}/companies/${id}`, values)},
     deleteCompany: (id: number) => { return axios.delete(`${BASE_URL_API}/companies/${id}`)},
-    postCompany: (id: number, data: any ) => { return axios.post(`${BASE_URL_API}/companies`, data)},
+    postCompany: (data: any ) => { return axios.post(`${BASE_URL_API}/companies`, data)},
 
     getUnits: () => { return axios.get(`${BASE_URL_API}/units`)},
     getUnit: (id: number) => { return axios.get(`${BASE_URL_API}/units/${id}`)},
-    putUnit: (id: number) => { return axios.put(`${BASE_URL_API}/units/${id}`)},
+    putUnit: (id: number, values: any) => { return axios.put(`${BASE_URL_API}/units/${id}`, values)},
     deleteUnit: (id: number) => { return axios.delete(`${BASE_URL_API}/units/${id}`)},
-    postUnit: (id: number, data: any ) => { return axios.post(`${BASE_URL_API}/units`, data)},
+    postUnit: (data: any ) => { return axios.post(`${BASE_URL_API}/units`, data)},
 
     getAssets: () => { return axios.get(`${BASE_URL_API}/assets`)},
     getAsset: (id: number) => { return axios.get(`${BASE_URL_API}/assets/${id}`)},
-    putAsset: (id: number) => { return axios.put(`${BASE_URL_API}/assets/${id}`)},
+    putAsset: (id: number, values: any) => { return axios.put(`${BASE_URL_API}/assets/${id}`, values)},
     deleteAsset: (id: number) => { return axios.delete(`${BASE_URL_API}/assets/${id}`)},
-    postAsset: (id: number, data: any ) => { return axios.post(`${BASE_URL_API}/assets`, data)},
+    postAsset: (data: any ) => { return axios.post(`${BASE_URL_API}/assets`, data)},
 
 }
 
